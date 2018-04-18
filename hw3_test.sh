7b4d5ce5e46ffc96c697bcbeabc9db2b07296ff3
Willy Tai <b04502139@ntu.edu.tw>
2018-04-18 13:00:51 +0800

    update hw3_test.sh

--- hw3_test.sh
+++
@@ -1,3 +0,0 @@
-wget -O merge_tmp.h5 'https://www.dropbox.com/s/43sv8rfywzf3k46/merge_tmp.h5?dl=1'
-wget -O merge.h5 'https://www.dropbox.com/s/wgj7wn9btczqmyg/merge.h5?dl=1'
-python3 test.py $1 $3 $2
